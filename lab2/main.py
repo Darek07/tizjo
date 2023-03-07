c0c40d8255096437809898a217c15db85475e41e
Darek07 <31630419+Darek07@users.noreply.github.com>
2023-03-07 17:51:18 +0100

    Swap case

--- lab2/main.py
+++ lab2/main.py
@@ -8,7 +8,8 @@ def my_printf(format_string,param):
     for idx in range(0,len(format_string)):
         if shouldDo:
             if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
+                sw_cs = param.swapcase()
+                print(sw_cs,end="")
                 shouldDo=False
             else:
                 print(format_string[idx],end="")

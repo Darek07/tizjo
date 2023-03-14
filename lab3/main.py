edb8631c7355e310a639a35a9845a5303a625411
Darek07 <darektsydik@gmail.com>
2023-03-14 18:19:42 +0100

    task implementation

--- lab3/main.py
+++ lab3/main.py
@@ -1,43 +1,29 @@
 #!/usr/bin/env python3
 
 import sys
+import re
 
 def my_printf(format_string,param):
-    #print(format_string)
-    shouldDo=True
-    len_format = False
-    for idx in range(0,len(format_string)):
-        if shouldDo:
-            len_format, length = is_length_format(format_string, idx)
-            if format_string[idx] == '#' and (format_string[idx+1] == 'k' or len_format == True):
-                if len_format == True:
-                    param = param[:length]
-                sw_cs = param.swapcase()
-                print(sw_cs,end="")
-                shouldDo=False
-            else:
-                print(format_string[idx],end="")
-        else:
-            if len_format and (format_string[idx] == '.' or is_digit(format_string[idx])):
-                continue
-            shouldDo=True
-    print("")
+    match = re.search(r'#([1-9]\d*)?(\.\d+)?k', format_string)
+
+    if not match:
+        print(format_string)
+        return
+
+    sp = param.swapcase()
+    r = match.group(0)
     
-def is_length_format(format_string, index):
-    if not (format_string[index] == '#' and format_string[index+1] == '.'):
-        return False, 0
-    number = ''
-    #print()
-    for idx in range(index+2, len(format_string)):
-        #print(format_string[idx])
-        if is_digit(format_string[idx]):
-            number += format_string[idx]
-        elif format_string[idx] == 'k':
-            return True, int(number)
-        else: return False, number
-        
-def is_digit(c):
-    return c in '0123456789'
+    first = match.group(1)
+    if first:
+        free_chars = int(first) - len(sp)
+        sp = " " * max(0, free_chars) + sp
+
+    second = match.group(2)
+    if second:
+        sp = sp[:int(second[1:])]
+
+    res = format_string.replace(r, sp)
+    print(res)
         
 
 data=sys.stdin.readlines()

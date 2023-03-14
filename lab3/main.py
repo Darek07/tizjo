e000598cdb23a597ad3c3e191267309f893b9db9
Darek07 <darektsydik@gmail.com>
2023-03-14 17:35:54 +0100

    Previous functionality

--- lab3/main.py
+++ lab3/main.py
@@ -5,16 +5,40 @@ import sys
 def my_printf(format_string,param):
     #print(format_string)
     shouldDo=True
+    len_format = False
     for idx in range(0,len(format_string)):
         if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
+            len_format, length = is_length_format(format_string, idx)
+            if format_string[idx] == '#' and (format_string[idx+1] == 'k' or len_format == True):
+                if len_format == True:
+                    param = param[:length]
+                sw_cs = param.swapcase()
+                print(sw_cs,end="")
                 shouldDo=False
             else:
                 print(format_string[idx],end="")
         else:
+            if len_format and (format_string[idx] == '.' or is_digit(format_string[idx])):
+                continue
             shouldDo=True
     print("")
+    
+def is_length_format(format_string, index):
+    if not (format_string[index] == '#' and format_string[index+1] == '.'):
+        return False, 0
+    number = ''
+    #print()
+    for idx in range(index+2, len(format_string)):
+        #print(format_string[idx])
+        if is_digit(format_string[idx]):
+            number += format_string[idx]
+        elif format_string[idx] == 'k':
+            return True, int(number)
+        else: return False, number
+        
+def is_digit(c):
+    return c in '0123456789'
+        
 
 data=sys.stdin.readlines()
 

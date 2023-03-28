ec10409d97fcb855ef0b7cab19fcc467a8ea1950
Darek07 <darektsydik@gmail.com>
2023-03-28 17:38:35 +0200

    change my_printf

--- lab4/main.py
+++ lab4/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+import re
 
 def reverse_num(param):
     return str(abs(param))
@@ -10,19 +11,17 @@ def add_sign_if_appears(param):
     return '-' + str(param) if param < 0 else str(param)
 
 
-def my_printf(format_string,param):
-    #print(format_string)
-    shouldDo=True
-    for idx in range(0,len(format_string)):
-        if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
-                shouldDo=False
-            else:
-                print(format_string[idx],end="")
-        else:
-            shouldDo=True
-    print("")
+def my_printf(format_string, param):
+    match = re.search('#g', format_string)
+
+    if not match:
+        print(format_string)
+        return
+
+    replacement = match.group(0)
+    param = add_sign_if_appears(param) + reverse_num(param)
+
+    print(format_string.replace(replacement, param))
 
 data=sys.stdin.readlines()
 

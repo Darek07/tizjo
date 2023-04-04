30fd72e7f79da00c0e881633ea19469122059d59
Darek07 <darektsydik@gmail.com>
2023-04-04 17:33:26 +0200

    sub each digit

--- lab5/main.py
+++ lab5/main.py
@@ -3,6 +3,27 @@
 import sys
 import re
 
+def sub_each_digit(param):
+    replacements = {
+        '-': '-',
+        '1': '0',
+        '2': '1',
+        '3': '2',
+        '4': '3',
+        '5': '4',
+        '6': '5',
+        '7': '6',
+        '8': '7',
+        '9': '8',
+        '0': '9'
+    }
+
+    res_array = [replacements[c] for c in str(param)]
+    res = ''.join(res_array)
+    
+    return res
+
+
 def my_printf(format_string,param):
     match = re.search(r'#(\d+)g', format_string)
 
@@ -11,6 +32,7 @@ def my_printf(format_string,param):
         return
 
     r = match.group(0)
+    param = sub_each_digit(param)
 
     first = match.group(1)
     if first:

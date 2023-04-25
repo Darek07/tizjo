eb0338c76dee3825b886690dceccfb8e7e5d66d7
Darek07 <darektsydik@gmail.com>
2023-04-25 17:29:56 +0200

    replace

--- lab7/main.py
+++ lab7/main.py
@@ -3,6 +3,15 @@
 import sys
 import re
 
+replacements = {
+    'a': 'g',
+    'b': 'h',
+    'c': 'i',
+    'd': 'j',
+    'e': 'k',
+    'f': 'l'
+}
+
 def my_printf(format_string,param):
     match = re.search(r'#j', format_string)
 
@@ -11,9 +20,11 @@ def my_printf(format_string,param):
         return
 
     r = match.group(0)
-    hex_param = hex(param)
+    hex_param = str(hex(int(param)))
+
+    replaced = ''.join([replacements[c] if c in replacements else c for c in hex_param])
     
-    res = format_string.replace(r, hex_param)
+    res = format_string.replace(r, replaced)
     print(res)
 
 data=sys.stdin.readlines()

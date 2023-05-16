6368ea40df9a61105d00daf2a8b5181fafce21df
Daryush Tsydzik <237559@edu.p.lodz.pl>
2023-05-16 17:36:32 +0200

    implementation

--- lab8/main.py
+++ lab8/main.py
@@ -9,24 +9,28 @@ replacements = {
     'c': 'i',
     'd': 'j',
     'e': 'k',
-    'f': 'l'
+    'f': 'l',
+    '0': 'o'
 }
 
 def my_printf(format_string,param):
-    match = re.search(r'#j', format_string)
+    match = re.search(r'#\.(\d+)j', format_string)
 
     if not match:
         print(format_string)
         return
 
     r = match.group(0)
+    length_min = match.group(1)
     
     if param.startswith('0x'):
         hex_param = str(param)[2:]
     else:
         hex_param = str(hex(int(param)))[2:]
 
-    replaced = ''.join([replacements[c] if c in replacements else c for c in hex_param.lower()])
+    param_w_zeros = max(0, int(length_min) - len(hex_param)) * '0' + hex_param
+
+    replaced = ''.join([replacements[c] if c in replacements else c for c in param_w_zeros.lower()])
     
     res = format_string.replace(r, replaced)
     print(res)

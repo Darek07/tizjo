014acda007d52ef96a8adeb5948c806a38fdd84e
Daryush Tsydzik <237559@edu.p.lodz.pl>
2023-06-06 17:23:19 +0200

    init implementation

--- lab11/main.py
+++ lab11/main.py
@@ -1,20 +1,37 @@
 #!/usr/bin/env python3
 
 import sys
+import re
 
-def my_printf(format_string,param):
-    #print(format_string)
-    shouldDo=True
-    for idx in range(0,len(format_string)):
-        if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
-                shouldDo=False
-            else:
-                print(format_string[idx],end="")
-        else:
-            shouldDo=True
-    print("")
+def convert(param):
+    binary = bin(param)[2:]
+    reversed_bin = binary[::-1]
+    res = ''
+    for i, bit in enumerate(reversed_bin):
+        res += '0' if bit == '0' else 'abcdefghij'[i % 10]
+
+    return res[::-1]
+
+
+def my_printf(format_string, param):
+    match = re.search(r'#b', format_string)
+
+    if not match:
+        print(format_string)
+        return
+
+    r = match.group(0)
+    
+    try:
+        param = int(param)
+    except Exception:
+        param = 0
+
+    converted = convert(param)
+    
+    res = format_string.replace(r, converted)
+    print(res)
+
 
 data=sys.stdin.readlines()
 
